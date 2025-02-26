5f8607786642e982bc0606980b643b8ecf0f403b
Marlos Emanuel <marlosemanuel28@gmail.com>
2025-02-26 08:23:03 -0300

    refactory: refatora o controller e o Service para a utilização do Request e Response e Mapper

--- Cadastro_de_Usuarios_backend/src/main/java/dev/marlosemanuel/Cadastro_de_Usuarios_backend/service/UsuarioService.java
+++ Cadastro_de_Usuarios_backend/src/main/java/dev/marlosemanuel/Cadastro_de_Usuarios_backend/service/UsuarioService.java
@@ -1,12 +1,16 @@
 package dev.marlosemanuel.Cadastro_de_Usuarios_backend.service;
 
+import dev.marlosemanuel.Cadastro_de_Usuarios_backend.mapper.UsuarioMapper;
 import dev.marlosemanuel.Cadastro_de_Usuarios_backend.model.Usuario;
 import dev.marlosemanuel.Cadastro_de_Usuarios_backend.repository.UsuarioRepository;
+import dev.marlosemanuel.Cadastro_de_Usuarios_backend.request.UsuarioRequest;
+import dev.marlosemanuel.Cadastro_de_Usuarios_backend.response.UsuarioResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -14,22 +18,29 @@ public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
 
-    public List<Usuario> findAll() {
-        return usuarioRepository.findAll();
+    public List<UsuarioResponse> findAll() {
+        return usuarioRepository.findAll().stream()
+                .map(UsuarioMapper::mapResponse)
+                .collect(Collectors.toList());
     }
 
-    public void save(Usuario usuario) {
+    public UsuarioResponse save(UsuarioRequest request) {
+        Usuario usuario = UsuarioMapper.mapEntity(request);
         usuarioRepository.save(usuario);
+        return UsuarioMapper.mapResponse(usuario);
     }
 
     public void delete(Long id) {
         usuarioRepository.deleteById(id);
     }
 
-    public Optional<Usuario> findById(Long id) {
-        return usuarioRepository.findById(id);
+    public Optional<UsuarioResponse> findById(Long id) {
+        return usuarioRepository.findById(id).map(UsuarioMapper::mapResponse);
     }
 
+
+
+    //Implementar no controller !!
     public Optional<Usuario> findByNome(String nome) {
         return usuarioRepository.findByNome(nome);
     }
